d893bab79ff2a62c2185a5534a7e33a6d3dc22d1
lazycattus <ainaqas167@gmail.com>
2025-05-27 15:38:10 +0800

    Fatimah punya

--- order_food.php
+++ order_food.php
@@ -306,7 +306,7 @@
 				                                	echo "<div class = 'item_select_part'>";
 				                                    	echo "<div class = 'menu_price_field'>";
 				    										echo "<span style = 'font-weight: bold;'>";
-				                                    			echo $row['menu_price']."$";
+				                                    			echo "RM".$row['menu_price'];
 				                                    		echo "</span>";
 				                                    	echo "</div>";
 				                                    ?>
