0fa2d671fad7c99af76fc0cb7f31377a8659c16e
jkl0vain <jkl0vain@gmail.com>
2025-06-18 16:14:50 +0800

    Update functions.php
    title website

--- Includes/functions/functions.php
+++ Includes/functions/functions.php
@@ -6,9 +6,9 @@
 	{
 		global $pageTitle;
 		if(isset($pageTitle))
-			echo $pageTitle." | Vincent Restaurant - Your Restaurant";
+			echo $pageTitle." | Orderlah Restaurant";
 		else
-			echo "Vincent Restaurant | Your Restaurant";
+			echo "Orderlah Restaurant | Your Restaurant";
 	}
 
 	/*
